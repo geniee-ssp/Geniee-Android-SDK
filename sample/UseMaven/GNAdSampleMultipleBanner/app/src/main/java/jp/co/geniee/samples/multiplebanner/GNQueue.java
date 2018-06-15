5ab67fadd0fc8650959e24a55612f2b2cc58b1c2
takaaki-kobayashi <takaaki.kobayashi@geniee.co.jp>
2018-06-15 12:37:50 +0900

    Maven implementation

---
+++ sample/UseMaven/GNAdSampleMultipleBanner/app/src/main/java/jp/co/geniee/samples/multiplebanner/GNQueue.java
@@ -0,0 +1,37 @@
+package jp.co.geniee.samples.multiplebanner;
+
+import java.util.ArrayList;
+
+public class GNQueue {
+	int maxSize;
+	ArrayList<Object> queue;
+	
+	public GNQueue(int maxSize) {
+		super();
+		this.maxSize = maxSize;
+		this.queue = new ArrayList<Object>();
+	}
+	
+	public Object dequeue() {
+		Object obj;
+		if (queue.size() == 0) return null;
+		obj = queue.get(0);
+		if (obj != null) {
+			queue.remove(0);
+		}
+		return obj;
+	}
+	
+	public void enqueue(Object obj) {
+		if (obj == null) return;
+		if (queue.size() >= maxSize) {
+			queue.remove(0);
+		}
+		queue.add(obj);
+	}
+	
+	public int size() {
+		return queue.size();
+	}
+
+}
---
+++ sample/UseMaven/GNAdSampleNativeAd/app/src/main/java/jp/co/geniee/samples/gnadsamplenativead/MyApplication.java
@@ -0,0 +1,14 @@
+package jp.co.geniee.samples.gnadsamplenativead;
+
+import android.app.Application;
+import jp.co.geniee.gnadsdk.common.GNAdSDK;
+
+public class MyApplication extends Application {
+    GNAdSDK gnadsdk = null;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        GNAdSDK gnadsdk = GNAdSDK.getInstatnce(getApplicationContext());
+        gnadsdk.init();
+    }
+}
