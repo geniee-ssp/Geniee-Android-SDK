903d8b3ba0ad151c410502594a11af11c3b678e6
takaaki-kobayashi <takaaki.kobayashi@geniee.co.jp>
2018-06-21 19:49:14 +0900

    Maven implementation

--- sample/UseMaven/GNAdSampleNativeAd/app/src/main/java/jp/co/geniee/samples/gnadsamplenativead/NativeAdSampleActivity.java
+++ sample/UseMaven/GNAdSampleNativeAd/app/src/main/java/jp/co/geniee/samples/gnadsamplenativead/NativeAdSampleActivity.java
@@ -60,7 +60,7 @@ public class NativeAdSampleActivity extends ListActivity implements GNNativeAdRe
 		timeStart = System.currentTimeMillis();
 
 		// Initialize SDK GNNativeAdRequest
-		nativeAdRequest = new GNNativeAdRequest(this, "YOUR_SSP_APP_ID");
+		nativeAdRequest = new GNNativeAdRequest(this, "1309479");
 		nativeAdRequest.setAdListener(this);
 		//nativeAdRequest.setGeoLocationEnable(true);
 		//nativeAdRequest.setLogPriority(GNAdLogger.INFO);
