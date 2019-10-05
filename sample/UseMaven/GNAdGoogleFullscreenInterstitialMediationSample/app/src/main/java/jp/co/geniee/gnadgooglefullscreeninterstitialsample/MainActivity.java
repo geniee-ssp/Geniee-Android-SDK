6f312f3f211abed60f0387a036c50e465e8c7e97
Kobayashi Takaaki <takaaki-kobayashi@GD0071.local>
2019-10-05 13:21:21 +0900

    support API29

--- sample/UseMaven/GNAdGoogleFullscreenInterstitialMediationSample/app/src/main/java/jp/co/geniee/gnadgooglefullscreeninterstitialsample/MainActivity.java
+++ sample/UseMaven/GNAdGoogleFullscreenInterstitialMediationSample/app/src/main/java/jp/co/geniee/gnadgooglefullscreeninterstitialsample/MainActivity.java
@@ -3,7 +3,7 @@ package jp.co.geniee.gnadgooglefullscreeninterstitialsample;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
